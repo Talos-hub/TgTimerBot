03a48e16e28a08710e36924c32457724b6fc6016
= <=>
2025-09-06 01:49:48 +0700

    fixed class: Interval

--- TgTimerBot/Models/Interval.cs
+++ TgTimerBot/Models/Interval.cs
@@ -6,17 +6,23 @@ using System.Threading.Tasks;
 
 namespace TgTimerBot.Models
 {
+    /// <summary>
+    /// Interval implemented Ifood
+    /// It contatins Hour, Minute, Second, TypeFood
+    /// </summary>
     public class Interval:IFood
     {
         public int Hour { get; set; }
         public int Minute { get; set; }
-        public int Second { get; set; }
+        public int Second { get; set; } 
+        public string Name { get; set; }
 
-        public Interval(int hour,int minute, int second) 
+        public Interval(int hour,int minute, int second, string typeFood) 
         {
             Hour = hour;
             Minute = minute;
             Second = second; 
+            Name = typeFood;
         }
 
     }
